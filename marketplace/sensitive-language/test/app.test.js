237f8651dddf75f6c1b74865246a5e044bb923af
Rouven Weßling <rouven@contentful.com>
2019-03-09 16:15:04 +0100

    More tests for the SensitiveLanguage extension

--- marketplace/sensitive-language/test/app.test.js
+++ marketplace/sensitive-language/test/app.test.js
@@ -9,7 +9,7 @@ jest.mock('../src/unsupported-language.js', () => ({
   UnsupportedLanguage: mockComponent('unsupported-language')
 }));
 jest.mock('../src/language-checker.js', () => ({
-  UnsupportedLanguage: mockComponent('language-checker')
+  LanguageChecker: mockComponent('language-checker')
 }));
 
 const {App} = require('../src/app.js');
@@ -24,6 +24,86 @@ test('App: bails out for non-english languages', function () {
     },
     window: {
       startAutoResizer() {}
+    }
+  };
+
+  const tree = renderer
+    .create(<App extension={extension} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('App: works for English languages', function () {
+  const extension = {
+    locales: {
+      default: 'en-US',
+      names: {
+        'en-US': 'English'
+      }
+    },
+    window: {
+      startAutoResizer() {}
+    },
+    contentType: {
+      fields: [
+        {
+          id: '123',
+          type: 'Symbol'
+        }
+      ]
+    },
+    entry: {
+      sys: {
+        id: 'ABC'
+      }
+    }
+  };
+
+  const tree = renderer
+    .create(<App extension={extension} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('App: filters out non-text fields', function () {
+  const extension = {
+    locales: {
+      default: 'en-US',
+      names: {
+        'en-US': 'English'
+      }
+    },
+    window: {
+      startAutoResizer() {}
+    },
+    contentType: {
+      fields: [
+        {
+          id: '123',
+          type: 'Symbol'
+        },
+        {
+          id: '456',
+          type: 'Number'
+        },
+        {
+          id: '789',
+          type: 'Text'
+        },
+        {
+          id: '012',
+          type: 'RichText'
+        },
+        {
+          id: '345',
+          type: 'Boolean'
+        }
+      ]
+    },
+    entry: {
+      sys: {
+        id: 'ABC'
+      }
     }
   };
 
